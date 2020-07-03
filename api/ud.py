5e6ef9034e21792df39150a4ed6b6fc047511276
Ayaan Zaidi <biblioklept@icloud.com>
2020-07-03 19:52:15 +0530

    [ud] fixed broken sort

--- api/ud.py
+++ api/ud.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import emoji
 import requests
 from telegram import Bot, Update
 
@@ -9,16 +10,16 @@ def ud(bot: Bot, update: Update):
     """
     Command to query UD for word definition
     """
-    message = update.message.text
+    message = update.message
 
     # Split after /ud command to get first argument
-    word = message.text.strip().split(' ', 1)
-    url = "http://api.urbandictionary.com/v0/define?term=" + word
+    word = message.text.strip().split(' ', 1)[1]
+    url = f'http://api.urbandictionary.com/v0/define?term={word}'
 
     res = requests.get(url)
     result = res.json()
 
-    if result is None:
+    if not result['list']:
         bot.send_message(
             chat_id=message.chat_id,
             text="No entry found.",
@@ -26,13 +27,21 @@ def ud(bot: Bot, update: Update):
         )
         return
 
-    result = result.sort()[0]
+    # Sort to get result with most thumbs up
+    max_thumbs, idx = 0, 0
+    for index, value in enumerate(result['list']):
+        if max_thumbs < value["thumbs_up"]:
+            idx = index
+            max_thumbs = value["thumbs_up"]
+
+    result = result['list'][idx]
 
     heading = result["word"]
     definition = result["definition"]
     example = result["example"]
+    thumbs = emoji.emojize(f':thumbs_up: × {max_thumbs}')
 
-    output = '*{heading}* \n {definition} \n {example}'
+    output = f'*{heading}*\n\n{definition}\n\n_{example}_\n\n`{thumbs}`'
 
     bot.send_message(
         chat_id=message.chat_id,
