88987adec96292bf7bca4240a76b11b24e972361
Ayaan Zaidi <biblioklept@icloud.com>
2020-07-07 02:15:19 +0530

    [translate] added dest language

--- api/translate.py
+++ api/translate.py
@@ -4,21 +4,38 @@ from googletrans import Translator
 
 def translate(bot: Bot, update: Update):
     message = update.message
-    sentence = ""
-    if len(message.text.strip().split(' ', 1)) >= 2:
+    sentence, dest = "", ""
+    if len(message.text.strip().split(' ', 2)) >= 2:
         sentence = message.text.strip().split(' ', 1)[1]
     else:
         bot.send_message(
             chat_id=message.chat_id,
             # blame Udit for this example
-            text="*Usage:* `/tl {SENTENCE}`\n*Example:* `/tl watashi wa anato no suki desu`",
+            text="*Usage:* `/tl {DEST} - {SENTENCE}`\n*Example:* `/tl en -watashi wa anato no suki desu`\nDefaults to "
+                 "`en` if none provided.",
             reply_to_message_id=message.message_id,
             parse_mode='Markdown'
         )
 
+    if '-' in sentence.split(' '):
+        to_translate = sentence.split(' ', 2)[2]
+        dest = sentence.split(' ', 2)[0]
+    else:
+        to_translate = sentence.split(' ', 2)[0]
+
     translator = Translator()
-    translated = translator.translate(sentence)
-    source = translator.detect(sentence)
+    if dest == "":
+        translated = translator.translate(to_translate)
+    else:
+        try:
+            translated = translator.translate(to_translate, dest=dest)
+        except ValueError:
+            bot.send_message(
+                chat_id=message.chat_id,
+                text="Invalid language.",
+                reply_to_message_id=message.message_id,
+            )
+            return
 
     output = translated.text
 
