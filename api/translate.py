507439210359d8c31e85aef064c432079b8b8f0a
Ayaan Zaidi <biblioklept@icloud.com>
2020-07-07 01:37:44 +0530

    [translate] init

--- api/translate.py
+++ api/translate.py
@@ -4,7 +4,17 @@ from googletrans import Translator
 
 def translate(bot: Bot, update: Update):
     message = update.message
-    sentence = message.text.strip().split(' ', 1)[1]
+    sentence = ""
+    if len(message.text.strip().split(' ', 1)) >= 2:
+        sentence = message.text.strip().split(' ', 1)[1]
+    else:
+        bot.send_message(
+            chat_id=message.chat_id,
+            # blame Udit for this example
+            text="*Usage:* `/tl {SENTENCE}`\n*Example:* `/tl watashi wa anato no suki desu`",
+            reply_to_message_id=message.message_id,
+            parse_mode='Markdown'
+        )
 
     translator = Translator()
     translated = translator.translate(sentence)
