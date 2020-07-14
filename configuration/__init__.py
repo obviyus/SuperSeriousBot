94afff68b76113bab2b8a6294926ab72801c815c
tyagdit <tyagdit@gmail.com>
2020-07-14 17:48:41 +0530

    Fix config init formatting

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -5,4 +5,7 @@ try:
         data = yaml.safe_load(config)
         config = data
 except FileNotFoundError:
-    raise FileNotFoundError("\nPlease create a 'config.yaml' file and put the credentials needed for the bot in it.\nRefer configuration/example_config.yaml for a template")
+    raise FileNotFoundError(
+            "\nPlease create a 'config.yaml' file and put the credentials needed for the bot in it.\n" \
+            "Refer configuration/example_config.yaml for a template"
+    )
