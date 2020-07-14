e8ea968251cd8cc8e3c595c6874cd0fdbec2479f
Udit <pudit2002@gmail.com>
2020-07-14 17:12:30 +0530

    deleted weather

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from api.tts import tts
 from api.hltb import hltb
 from api.countdown import countdown
 from api.time import time
+from api.countdown import countdown
 
 from chat_management.kick import kick
 
@@ -41,7 +42,10 @@ def main():
     calc_hanlder = CommandHandler('calc', calc)
     tts_handler = CommandHandler('tts', tts)
     time_handler = CommandHandler('time', time)
-
+    countdown_handler = CommandHandler('countdown', countdown)
+    calc_hanlder = CommandHandler('calc', calc)
+    tts_handler = CommandHandler('tts', tts)
+    
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(kick_handler)
     dispatcher.add_handler(ud_handler)
@@ -52,6 +56,9 @@ def main():
     dispatcher.add_handler(calc_hanlder)
     dispatcher.add_handler(tts_handler)
     dispatcher.add_handler(time_handler)
+    dispatcher.add_handler(countdown_handler)
+    dispatcher.add_handler(calc_hanlder)
+    dispatcher.add_handler(tts_handler)
 
     updater.start_polling()
     updater.idle()
