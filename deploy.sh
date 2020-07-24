42cc3da351359ba83f0e472fff940bedf05f2da3
Nanda Gopal <VNandaGopal@pm.me>
2020-07-25 03:40:43 +0530

    Deploy & run bot on server

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/travis-testing
-ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S WeirdIndianBot quit && screen -S WeirdIndianBot -dm bash -c 'cd travis-testing; python3 main.py'"
+ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S WeirdIndianBot quit || true && screen -S WeirdIndianBot -dm bash -c 'cd travis-testing; python3 main.py'"
