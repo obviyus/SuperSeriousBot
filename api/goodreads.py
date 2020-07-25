7b9107393ed11381f5eec52da6cbd4403b586b60
Ayaan Zaidi <biblioklept@icloud.com>
2020-07-26 00:08:52 +0530

    [goodreads] remove for loops

--- api/goodreads.py
+++ api/goodreads.py
@@ -7,23 +7,21 @@ def goodreads(update, context):
     """ Command to query GoodReads for a book"""
     message = update.message
     query = ' '.join(context.args)
+    parse_mode = 'Markdown'
 
     if not query:
-        text = "*Usage:* `/gr {BOOK_NAME}`\n"\
+        text = "*Usage:*   `/gr {BOOK_NAME}`\n"\
                "*Example:* `/gr stoner`"
-        parse_mode = 'Markdown'
     else:
         response = get(f'https://www.goodreads.com/search.xml?key={config["GOODREADS_API_KEY"]}&q={query}')
         root = ET.fromstring(response.content)
+        id = root.findtext("search/results/work/best_book/id")
 
-        for node in root.iter('work'):
-            if node is not None:
-                id = node.find('best_book').find('id').text
-                text = make_result(id)
-                parse_mode = 'HTML'
-                break
-            else:
-                text = "No entry found."
+        if id is not None:
+            text = make_result(id)
+            parse_mode = 'HTML'
+        else:
+            text = "No entry found."
 
     message.reply_text(
         text=text,
@@ -37,19 +35,21 @@ def make_result(goodreads_id):
     r = get(f'https://www.goodreads.com/book/show.xml?key={config["GOODREADS_API_KEY"]}&id={goodreads_id}')
     root = ET.fromstring(r.content)
 
-    for node in root.iterfind('book'):
-        title = node.find('title').text
-        isbn = node.find('isbn13').text
-        year = node.find('publication_year').text
-        author = node.find('authors').find('author').find('name').text
-        description = node.find('description').text.replace("<br />", "")
-        description = description[:description.index('.', 200) + 1]
-        cover_url = f"http://covers.openlibrary.org/b/isbn/{isbn}-L.jpg"
-        pages = node.find('num_pages').text
-        url = node.find('url').text
-        stars = f"⭐ {node.find('average_rating').text}"
+    node = root.find('book')
+    title = node.findtext('title')
+    isbn = node.findtext('isbn13')
+    year = node.findtext('publication_year')
+    author = node.findtext('authors/author/name')
 
-        return f"<b>{title}</b> - ({year})\n"\
-               f"<a href='{cover_url}'>&#8205;</a>"\
-               f"{author}\n{stars} 📖 {pages} pages 🔗 <a href='{url}'>Goodreads</a>\n"\
-               f"\n{description}"
+    description = node.findtext('description').replace("<br />", "")
+    description = description[:description.index('.', 200) + 1]
+
+    cover_url = f"http://covers.openlibrary.org/b/isbn/{isbn}-L.jpg"
+    pages = node.find('num_pages').text
+    url = node.find('url').text
+    stars = f"⭐ {node.find('average_rating').text}"
+
+    return  f"<b>{title}</b> - ({year})\n"\
+            f"<a href='{cover_url}'>&#8205;</a>"\
+            f"{author}\n{stars} 📖 {pages} pages 🔗 <a href='{url}'>Goodreads</a>\n"\
+            f"\n{description}"
