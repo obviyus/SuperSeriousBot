9f86a6408f1b6a00b4aad5ef6deb09a7a32ac1d5
Nanda Gopal <VNandaGopal@pm.me>
2020-07-26 00:40:15 +0530

    TravisCI deploy to master and testing

--- scripts/deploy_master.sh
+++
@@ -1,2 +0,0 @@
-scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/bot
-ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S superseriousbot quit || true && screen -S superseriousbot -dm bash -c 'cd bot; python3 main.py'"
--- scripts/deploy_testing.sh
+++
@@ -1,2 +0,0 @@
-scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/travis-testing
-ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S weirdindianbot quit || true && screen -S weirdindianbot -dm bash -c 'cd travis-testing; python3 main.py'"
