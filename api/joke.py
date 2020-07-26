acd9701fe7376ab76a7441e085ddd9525b8d9bd4
Ayaan Zaidi <biblioklept@icloud.com>
2020-07-27 01:35:41 +0530

    [joke] add another joke API

--- api/joke.py
+++ api/joke.py
@@ -1,16 +1,35 @@
 from requests import get
 import time
+import random
 
 
 def joke(update, context):
     """ Command to return a random joke"""
 
-    response = get('https://sv443.net/jokeapi/v2/joke/Any')
+    # Randomly choose between these two APIs
+    if random.random() < 0.5:
+        response = get('https://sv443.net/jokeapi/v2/joke/Any')
+    else:
+        response = get('https://official-joke-api.appspot.com/random_joke')
+
     response = response.json()
 
     update.message.reply_text(text=response["setup"])
     time.sleep(2.0)
+
+    try:
+        punchline = response["delivery"]
+    except KeyError:
+        punchline = response["punchline"]
+
     context.bot.send_message(
-        text=response["delivery"],
+        text=punchline[:-1] + " 😆",
         chat_id=update.message.chat_id
     )
+
+    # Randomly say this
+    if random.random() < 0.01:
+        context.bot.send_message(
+            text="Please don’t kick me 👉👈",
+            chat_id=update.message.chat_id
+        )
