0ca32ce8f0cb515675805b32588b56b0542e1ea8
Nanda Gopal <VNandaGopal@pm.me>
2020-07-27 19:01:42 +0530

    Update deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/bot
-ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S superseriousbot quit || true && screen -S superseriousbot -dm bash -c 'cd bot; git pull origin $3; python3 main.py'"
+ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S $2 quit || true && screen -S $2 -dm bash -c 'cd $1; git pull origin $3; python3 main.py'"
