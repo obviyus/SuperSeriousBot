309b2730959a0dd2d39384174c3af5f1ee33ea7a
tyagdit <tyagdit@gmail.com>
2020-07-27 18:19:57 +0530

    Experimenting with travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/$1
-ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S $2 quit || true && screen -S $2 -dm bash -c 'cd $1; python3 main.py'"
+#scp -r $TRAVIS_BUILD_DIR TG-bot@$HOSTNAME:/home/TG-bot/bot
+ssh -i /tmp/deploy_rsa TG-bot@$HOSTNAME  "screen -X -S superseriousbot quit || true && screen -S superseriousbot -dm bash -c 'cd bot; git pull origin master; python3 main.py'"
