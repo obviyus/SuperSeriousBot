4fc16ea9a2ec4391bd1b112eae31e5133b98cd2f
Ayaan Zaidi <biblioklept@icloud.com>
2020-11-20 20:34:32 +0530

    [docker] remove docker workflow

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -4,22 +4,22 @@ import os
 
 try:
     # By default, try to look for API keys in environment variables
-    data = {}
-    data["TELEGRAM_BOT_TOKEN"] = os.environ["TELEGRAM_BOT_TOKEN"]
-    data["WOLFRAM_APP_ID"] = os.environ["WOLFRAM_APP_ID"]
-    data["OPENWEATHER_API_KEY"] = os.environ["OPENWEATHER_API_KEY"]
-    data["GOODREADS_API_KEY"] = os.environ["GOODREADS_API_KEY"]
-    data["AIRVISUAL_API_KEY"] = os.environ["AIRVISUAL_API_KEY"]
-    data["JOGI_FILE_ID"] = os.environ["JOGI_FILE_ID"]
-    data["FOR_WHAT_ID"] = os.environ["FOR_WHAT_ID"]
-    data["PUNYA_SONG_ID"] = os.environ["PUNYA_SONG_ID"]
+    config = {}
+    config["TELEGRAM_BOT_TOKEN"] = os.environ["TELEGRAM_BOT_TOKEN"]
+    config["WOLFRAM_APP_ID"] = os.environ["WOLFRAM_APP_ID"]
+    config["OPENWEATHER_API_KEY"] = os.environ["OPENWEATHER_API_KEY"]
+    config["GOODREADS_API_KEY"] = os.environ["GOODREADS_API_KEY"]
+    config["AIRVISUAL_API_KEY"] = os.environ["AIRVISUAL_API_KEY"]
+    config["JOGI_FILE_ID"] = os.environ["JOGI_FILE_ID"]
+    config["FOR_WHAT_ID"] = os.environ["FOR_WHAT_ID"]
+    config["PUNYA_SONG_ID"] = os.environ["PUNYA_SONG_ID"]
 except KeyError:
     try:
         with open("config.yaml", 'r') as config:
-            data = yaml.safe_load(config)
-            config = data
+            config = yaml.safe_load(config)
     except FileNotFoundError:
         raise FileNotFoundError(
             "\nPlease create a 'config.yaml' file and put the credentials needed for the bot in it.\n"
-            "Refer configuration/example_config.yaml for a template"
+            "Refer configuration/example_config.yaml for a template.\n"
+            "You can alternatively set environmental variables."
         )
