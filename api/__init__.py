888f28747a6adbee4bbb3c0a317c7c1769f1fa61
Ayaan Zaidi <biblioklept@icloud.com>
2020-12-24 12:45:10 +0530

    misc. clean up

--- api/__init__.py
+++ api/__init__.py
@@ -1,25 +1,25 @@
 """Commands for general use."""
 from .age import age
+from .animals import shiba, fox, cat, catfact
 from .audio import jogi, forwhat, pon
 from .calc import calc
 from .caption import caption
 from .countdown import countdown
 from .currency import currency
+from .goodreads import goodreads
 from .hltb import hltb
+from .insult import insult
+from .joke import joke
+from .pfp import pad_image
+from .pic import pic
+from .qr import make
+from .response import wink, pat, hug
 from .sed import sed
+from .spurdo import spurdo
+from .stats import print_stats, increment, clear
 from .time import time
 from .translate import translate
 from .tts import tts
 from .ud import ud
-from .stats import print_stats, increment, clear
-from .goodreads import goodreads
-from .weather import weather
-from .animals import shiba, fox, cat, catfact
-from .insult import insult
-from .response import wink, pat, hug
-from .joke import joke
-from .qr import make
-from .pic import pic
-from .pfp import pad_image
 from .wait import wait
-from .spurdo import spurdo
+from .weather import weather
