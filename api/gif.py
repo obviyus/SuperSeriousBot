371a78dfb9a5d335ab5844c4dc28511e7ccd2e27
Ayaan Zaidi <biblioklept@icloud.com>
2021-02-24 17:36:23 +0530

    [gif] fix missing API key

--- api/gif.py
+++ api/gif.py
@@ -1,14 +1,16 @@
 import requests
+from configuration import config
 
 
 def gif(update, context):
     """Get a random GIF from giphy"""
-    params = {'api_key': ['GIPHY_API_KEY']}
+    params = {'api_key': config['GIPHY_API_KEY']}
     url = 'http://api.giphy.com/v1/gifs/random'
 
     response = requests.get(url, params=params)
-    print(response.json()['data']['url'])
-    update.message.reply_photo(
-        photo=response.json['data']['url'],
+    url = response.json()['data']['images']['original']['url']
+
+    update.message.reply_animation(
+        animation=url,
     )
     
