47b29f5bf343013a02e6539b9aae8504a9a98931
Ayaan Zaidi <biblioklept@icloud.com>
2021-03-14 17:11:24 +0530

    [config] re-order items in configuration

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -6,19 +6,16 @@ import yaml
 try:
     # By default, try to look for API keys in environment variables
     config: Dict[str, Union[str, list]] = {
+        "TELEGRAM_BOT_TOKEN": os.environ["TELEGRAM_BOT_TOKEN"],
         "AUDIO_RESTORE_USERS": os.environ["AUDIO_RESTORE_USERS"].split(),
         "CLIMACELL_API_KEY": os.environ["CLIMACELL_API_KEY"],
         "CLOUDMERSIVE_API_KEY": os.environ["CLOUDMERSIVE_API_KEY"],
         "FOR_WHAT_ID": os.environ["FOR_WHAT_ID"],
         "GOODREADS_API_KEY": os.environ["GOODREADS_API_KEY"],
         "JOGI_FILE_ID": os.environ["JOGI_FILE_ID"],
-        "MYSQL_IP_ALIAS": os.environ["MYSQL_IP_ALIAS"],
-        "MYSQL_PASSWORD": os.environ["MYSQL_PASSWORD"],
-        "MYSQL_USER": os.environ["MYSQL_USER"],
         "OPENWEATHER_API_KEY": os.environ["OPENWEATHER_API_KEY"],
         "PUNYA_SONG_ID": os.environ["PUNYA_SONG_ID"],
         "STEAM_API_KEY": os.environ["STEAM_API_KEY"],
-        "TELEGRAM_BOT_TOKEN": os.environ["TELEGRAM_BOT_TOKEN"],
         "WOLFRAM_APP_ID": os.environ["WOLFRAM_APP_ID"],
     }
 except KeyError:
