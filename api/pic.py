5fcf8952918ac1b778749e810786d16692420c4a
Ayaan Zaidi <zaidi.ayaan@gmail.com>
2021-06-11 14:26:20 +0530

    [pic] allow requesting more than 1 image

--- api/pic.py
+++ api/pic.py
@@ -1,7 +1,8 @@
 from random import choices
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 import requests
+from telegram import InputMediaPhoto
 
 if TYPE_CHECKING:
     import telegram
@@ -9,18 +10,30 @@ if TYPE_CHECKING:
 
 
 def pic(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> None:
-    """Get a random image from imgur"""
+    """Get random images from imgur"""
     if not update.message:
         return
 
-    chars: str = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'
+    chars: str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'
     length: int = 5
 
-    while True:
-        address: str = 'https://i.imgur.com/' + "".join(choices(chars, k=length)) + '.jpg'
-        if requests.get(address).url == address:
-            break
+    try:
+        n: int = int(context.args[0]) if context.args else 1
+        # Request maximum 5 images at once
+        n = min(n, 5)
+    except TypeError:
+        n = 1
 
-    update.message.reply_photo(
-        photo=address,
+    media_group: List[InputMediaPhoto] = []
+    while n > 0:
+        address: str = 'https://i.imgur.com/' + "".join(choices(chars, k=length)) + '.jpg'
+        r = requests.get(address)
+
+        # Ignore any images < 1000B
+        if r.url == address and int(r.headers['content-length']) > 1000:
+            media_group.append(InputMediaPhoto(media=address))
+            n -= 1
+
+    update.message.reply_media_group(
+        media=media_group,
     )
