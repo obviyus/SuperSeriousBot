e2112738039a00da310ccc77025a6869eef8b824
Nanda Gopal <VNandaGopal@pm.me>
2021-10-15 11:14:39 +0530

    [api] cover some use cases

--- api/dice.py
+++ api/dice.py
@@ -15,17 +15,17 @@ def dice(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> 
         return
     
     result: str
-    rollList = []
+    roll_list = []
     roll: str = ' '.join(context.args)
-    die = re.compile('([1-9]\d*)?d([1-9]\d*)([\+\-][1-9]\d*)?').match(roll)
+    die = re.compile('([1-9]\d*)?[d|D]([1-9]\d*)([\+\-][1-9]\d*)?').match(roll)
 
     if not die:
         result = "*Usage:* `/dice XdY +/- Z`\n" \
-                      "Rolls a die of Y sides X times, gets the sum and adds or subtracts a modifier Z \n" \
+                      "Rolls a die of Y sides X (default is 1) times, gets the sum and adds or subtracts a modifier Z (optional)\n" \
                       "*Example:* `/dice 2d20+4`\n"
     else:
-        for i in range(1 if not die.group(1) else int(die.group(1))):
-            rollList.append(random.randint(1,int(die.group(2))))
-        result = f"Roll: [{rollList}]\nResult = {sum(rollList) if not die.group(3) else sum(rollList) + int(die.group(3))}"
+        for i in range(int(die.group(1) or 1)):
+            roll_list.append(random.randint(1,int(die.group(2))))
+        result = f"Roll: [{roll_list}]\nResult = {sum(roll_list) + int(die.group(3) or 0)}"
         
     message.reply_text(text=result)       
