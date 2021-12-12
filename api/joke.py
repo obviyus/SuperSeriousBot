867a9fe226def5f669ded130e42040696dc3d558
Ayaan Zaidi <zaidi.ayaan@gmail.com>
2021-12-12 14:19:27 +0530

    [joke] remove broken joke API

--- api/joke.py
+++ api/joke.py
@@ -16,37 +16,20 @@ def joke(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> 
     else:
         return
 
-    # Randomly choose between these two APIs
-    response: Dict
-    if random.random() < 0.5:
-        response = get('https://v2.jokeapi.dev/joke/Any?type=twopart').json()
-    else:
-        response = get('https://official-joke-api.appspot.com/random_joke').json()
-
-    text: str
-    punchline: str
+    response: Dict = get('https://v2.jokeapi.dev/joke/Any?type=twopart').json()
+    punchline: str = response["delivery"]
 
     message.reply_text(text=response["setup"])
     time.sleep(2.0)
-
-    try:
-        punchline = response["delivery"]
-    except KeyError:
-        punchline = response["punchline"]
-
-    # Workaround to prevent last letter being replaced
-    if punchline[-1] != '.':
-        punchline += '.'
 
     context.bot.send_message(
         text=punchline[:-1] + " 😆",
         chat_id=message.chat_id
     )
 
-    time.sleep(2.0)
-
     # Randomly say this
     if random.random() < 0.01:
+        time.sleep(2.0)
         context.bot.send_message(
             text="Please don’t kick me 👉👈",
             chat_id=message.chat_id
