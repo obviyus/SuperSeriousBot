3e725d6323da41fbf9d0fb17d3475835c9b03e9c
Tyagraj Desigar <tyagdit@gmail.com>
2021-12-30 10:54:25 +0530

    Refactor /dl using yt-dlp

--- links/dl.py
+++ links/dl.py
@@ -1,49 +1,81 @@
-from io import BytesIO
 from typing import TYPE_CHECKING
+from logging import getLogger
 
-import requests
-import youtube_dl
+import yt_dlp
+from io import BytesIO
+from requests import get
 from telegram import MessageEntity
+from telegram.error import BadRequest
 
 if TYPE_CHECKING:
     import telegram
     import telegram.ext
 
-# Get download URL on a best effort basis; i.e. highest possible quality while keeping size under 50MB.
 ydl_opts = {
-    'format': 'best[filesize<50M]',
+    # b = best within the applied constraints
+    # TG Bots can't send docs/vids bigger than 50MB
+    # 3gp is terrible and makes tg servers unhappy
+    "format": "b[filesize<50M][ext!=3gp]",
+    "outtmpl": "-",
+    "logger": getLogger(),
+    "skip_download": True,
+    "age_limit": 33,
+    "geo_bypass": True,
 }
 
+class DLBufferUsedWarning(Exception):
+    pass
 
-def dl(update: 'telegram.Update', _context: 'telegram.ext.CallbackContext') -> None:
+
+def dl(update: 'telegram.Update', _: 'telegram.ext.CallbackContext') -> None:
     """Download a given video link and send as native Telegram video file"""
-    if update.message:
-        message: 'telegram.Message' = update.message
+    message: 'telegram.Message'
+    if update.message.reply_to_message:
+        message = update.message.reply_to_message
+    elif update.message:
+        message = update.message
     else:
         return
 
-    try:
-        video_url = list(message.reply_to_message.parse_entities([MessageEntity.URL]).values())[0]
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            try:
-                r = ydl.extract_info(video_url, download=False)
-            except youtube_dl.utils.DownloadError:
-                message.reply_text(
-                    text="No available video file under 50MB."
-                )
+    entities: list = list(message.parse_entities([MessageEntity.URL]).values())
 
-        # Telegram imposes a hard limit of 50MB on all document types
-        # Direct HTTP url to video is limited to 20MB
-        buffer = BytesIO()
-        buffer.write(requests.get(r['url'], stream=True).content)
-        buffer.seek(0)
-
-        message.reply_video(
-            video=buffer
-        )
-
-    except AttributeError:
-        message.reply_text(
-            text="*Usage:* `/dl` in reply to a link. Video file-size limited to < 50MB. The bot will download videos "
+    if entities:
+        video_url = entities[0]
+    else:
+        update.message.reply_text(
+            text="*Usage:* `/dl {LINK}` or reply to a link. Video file-size limited to < 50MB. The bot will download videos "
                  "on a best effort basis, i.e. highest possible quality while staying within the limit. "
         )
+        return
+
+    text: str
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            # age and geo restriction should be bypassed in most cases
+            vid_info = ydl.extract_info(video_url)
+            text = ""
+        except yt_dlp.utils.DownloadError as e:
+            vid_info = None
+            if e.msg and "Unsupported URL" in e.msg:
+                text = "Unsupported URL"
+            elif e.msg and "Requested format is not available" in e.msg:
+                text = "No video available under 50 MB (Telegram allows only 50 MB uploads for bots)"
+            else:
+                text = "Could not download video"
+
+    if text or not vid_info:
+        update.message.reply_text(text or "Could not download video")
+    else:
+        try:
+            update.message.reply_video(vid_info["url"])
+        except BadRequest:
+            # some vid URLs don't work god knows why, so falling back to using a file buffer
+            sent_msg = message.reply_text("Uploading...")
+            buffer = BytesIO()
+            buffer.write(get(vid_info['url'], stream=True).content)
+            buffer.seek(0)
+            sent_msg.delete()
+            update.message.reply_video(buffer)
+            buffer.close()
+            # log that a file buffer was used insteadd of a URL
+            raise DLBufferUsedWarning("Couldn't use URL for /dl, used a file buffer instead")
