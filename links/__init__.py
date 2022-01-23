4348cb46528cc8aba7c282fb94360086768210de
Ayaan Zaidi <zaidi.ayaan@gmail.com>
2022-01-23 19:30:35 +0530

    [links] add comments to links

--- links/__init__.py
+++ links/__init__.py
@@ -1,2 +1,3 @@
 """Link handler functions"""
 from .dl import dl
+from .reddit_comment import comment
---
+++ links/reddit_comment.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+import praw
+import logging
+from telegram import MessageEntity
+from configuration import config
+
+if TYPE_CHECKING:
+    import telegram
+    import telegram.ext
+
+reddit = praw.Reddit(
+    client_id=config["REDDIT_CLIENT_ID"],
+    client_secret=config["REDDIT_CLIENT_SECRET"],
+    user_agent=config["REDDIT_USER_AGENT"],
+)
+
+
+def comment(update: "telegram.Update", _context: "telegram.ext.CallbackContext") -> None:
+    """Replies to a link with the top comment posted on Reddit"""
+    message = update.message
+
+    entities: list = list(message.parse_entities([MessageEntity.URL]).values())
+    if entities:
+        original_url = entities[0]
+    else:
+        return
+
+    for submission in reddit.subreddit("all").search(
+        f'url:"{original_url}"',
+        sort="top",
+    ):
+        submission.comments.replace_more(limit=0)
+        for top_level_comment in submission.comments:
+            if top_level_comment.stickied:
+                continue
+
+            text = f"{top_level_comment.body}\n\n<a href='https://reddit.com{top_level_comment.permalink}'>- {top_level_comment.author.name}</a>"
+            logging.info(text)
+            message.reply_text(
+                text=text,
+                parse_mode='html',
+                disable_web_page_preview=True
+            )
+            return
