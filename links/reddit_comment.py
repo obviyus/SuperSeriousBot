45177f276e21a95da25bd9ab5b23f062486da1e7
Ayaan Zaidi <zaidi.ayaan@gmail.com>
2022-01-23 19:43:39 +0530

    [links] import praw instance

--- links/reddit_comment.py
+++ links/reddit_comment.py
@@ -1,17 +1,10 @@
 from typing import TYPE_CHECKING
-import praw
 from telegram import MessageEntity
-from configuration import config
+from ..api import reddit
 
 if TYPE_CHECKING:
     import telegram
     import telegram.ext
-
-reddit = praw.Reddit(
-    client_id=config["REDDIT_CLIENT_ID"],
-    client_secret=config["REDDIT_CLIENT_SECRET"],
-    user_agent=config["REDDIT_USER_AGENT"],
-)
 
 
 def comment(
