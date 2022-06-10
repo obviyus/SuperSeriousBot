565f83eb291b88b4d6f530cd187fced0571b50dc
Ayaan Zaidi <biblioklept@icloud.com>
2022-06-10 12:32:49 +0530

    feat(pic): revert /pic to old version

--- api/pic.py
+++ api/pic.py
@@ -1,9 +1,7 @@
 from random import choices
 from typing import List, TYPE_CHECKING
-
 import requests
-import random
-from configuration import config
+from telegram import InputMediaPhoto
 
 if TYPE_CHECKING:
     import telegram
@@ -15,17 +13,28 @@ def pic(update: "telegram.Update", context: "telegram.ext.CallbackContext") -> N
     if not update.message:
         return
 
-    albums = requests.get(
-        f"https://api.imgur.com/3/gallery/random/random/{random.randint(0, 100000)}",
-        headers={"Authorization": f"Client-ID {config['IMGUR_KEY']}"},
-    ).json()["data"]
-    random_album = random.choice(albums)
-    try:
-        image_url = random.choice(random_album["images"])["link"]
-    except KeyError:
-        # Some albums are just a single image
-        image_url = random_album["link"]
+    chars: str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz"
+    length: int = 5
 
-    update.message.reply_photo(
-        photo=image_url,
+    try:
+        n: int = int(context.args[0]) if context.args else 1
+        # Request maximum 5 images at once
+        n = min(n, 5)
+    except ValueError:
+        n = 1
+
+    media_group: List[InputMediaPhoto] = []
+    while n > 0:
+        address: str = (
+            "https://i.imgur.com/" + "".join(choices(chars, k=length)) + ".jpg"
+        )
+        r = requests.get(address)
+
+        # Ignore any images < 1000B
+        if r.url == address and int(r.headers["content-length"]) > 1000:
+            media_group.append(InputMediaPhoto(media=address))
+            n -= 1
+
+    update.message.reply_media_group(
+        media=media_group,
     )
