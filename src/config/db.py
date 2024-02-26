41e434857578ac1b22d441a6eba74f5003196733
Ayaan Zaidi <zaidi@uplause.io>
2024-02-26 15:11:33 +0530

    fix(db): allow configurable DB path prefix

--- src/config/db.py
+++ src/config/db.py
@@ -3,18 +3,9 @@ import sqlite3
 
 import redis
 
-
-def is_docker():
-    path = "/proc/self/cgroup"
-    return (
-        os.path.exists("/.dockerenv")
-        or os.path.isfile(path)
-        and any("docker" in line for line in open(path))
-    )
-
-
-PRIMARY_DB_PATH = f"{os.getcwd() if not is_docker() else '/db'}/SuperSeriousBot.db"
-INDIA_LAW_DB_PATH = f"{os.getcwd() if not is_docker() else '/db'}/IndiaLaw.db"
+DATABASE_PATH_PREFIX = os.environ.get("DATABASE_PATH_PREFIX", os.getcwd())
+PRIMARY_DB_PATH = f"{DATABASE_PATH_PREFIX}/SuperSeriousBot.db"
+INDIA_LAW_DB_PATH = f"{DATABASE_PATH_PREFIX}/IndiaLaw.db"
 
 sqlite_conn = sqlite3.connect(
     PRIMARY_DB_PATH,
@@ -32,7 +23,7 @@ sqlite_conn.row_factory = sqlite3.Row
 sqlite_conn_law_database.row_factory = sqlite3.Row
 
 redis = redis.StrictRedis(
-    host=f"{os.environ.get('REDIS_HOST', None) if os.environ.get('REDIS_HOST', None) else 'redis' if is_docker() else '127.0.0.1'}",
+    host=f"{os.environ.get('REDIS_HOST', '127.0.0.1')}",
     port=int(os.environ.get("REDIS_PORT", 6379)),
     db=0,
     decode_responses=True,
