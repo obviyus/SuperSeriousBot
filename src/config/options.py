ec889323f688900df7b97bf44bb55d435ce485f9
Ayaan Zaidi <zaidi@uplause.io>
2025-12-17 18:40:49 +0530

    refactor: remove unused key checks

--- src/config/options.py
+++ src/config/options.py
@@ -13,23 +13,13 @@ config: dict[str, Any]
 
 
 class APIConfig(BaseModel):
+    COBALT_URL: str | None = ""
     GIPHY_API_KEY: str | None = ""
     GOODREADS_API_KEY: str | None = ""
-    SMMRY_API_KEY: str | None = ""
-    STEAM_API_KEY: str | None = ""
-    WOLFRAM_APP_ID: str | None = ""
-    INSTAGRAM_SESSION_NAME: str | None = ""
-    IMGUR_API_KEY: str | None = ""
-    OPEN_AI_API_KEY: str | None = ""
-    YOUTUBE_API_KEY: str | None = ""
-    RAPID_API_KEY: str | None = ""
-    WINDY_API_KEY: str | None = ""
     NANO_GPT_API_KEY: str | None = ""
-    EMBEDEZ_API_KEY: str | None = ""
     OPENROUTER_API_KEY: str | None = ""
-    GOOGLE_API_KEY: str | None = ""
-    COBALT_URL: str | None = ""
     WAQI_API_KEY: str | None = ""
+    WOLFRAM_APP_ID: str | None = ""
 
 
 class TelegramConfig(BaseModel):
@@ -74,23 +64,13 @@ try:
             "QUOTE_CHANNEL_ID": quote_channel_id,
         },
         API={  # type: ignore[arg-type]
+            "COBALT_URL": os.environ.get("COBALT_URL", ""),
             "GIPHY_API_KEY": os.environ.get("GIPHY_API_KEY", ""),
             "GOODREADS_API_KEY": os.environ.get("GOODREADS_API_KEY", ""),
-            "IMGUR_API_KEY": os.environ.get("IMGUR_API_KEY", ""),
-            "INSTAGRAM_SESSION_NAME": os.environ.get("INSTAGRAM_SESSION_NAME", ""),
-            "RAPID_API_KEY": os.environ.get("RAPID_API_KEY", ""),
-            "SMMRY_API_KEY": os.environ.get("SMMRY_API_KEY", ""),
-            "STEAM_API_KEY": os.environ.get("STEAM_API_KEY", ""),
-            "OPEN_AI_API_KEY": os.environ.get("OPEN_AI_API_KEY", ""),
-            "WOLFRAM_APP_ID": os.environ.get("WOLFRAM_APP_ID", ""),
-            "WINDY_API_KEY": os.environ.get("WINDY_API_KEY", ""),
-            "YOUTUBE_API_KEY": os.environ.get("YOUTUBE_API_KEY", ""),
             "NANO_GPT_API_KEY": os.environ.get("NANO_GPT_API_KEY", ""),
-            "EMBEDEZ_API_KEY": os.environ.get("EMBEDEZ_API_KEY", ""),
             "OPENROUTER_API_KEY": os.environ.get("OPENROUTER_API_KEY", ""),
-            "GOOGLE_API_KEY": os.environ.get("GOOGLE_API_KEY", ""),
-            "COBALT_URL": os.environ.get("COBALT_URL", ""),
             "WAQI_API_KEY": os.environ.get("WAQI_API_KEY", ""),
+            "WOLFRAM_APP_ID": os.environ.get("WOLFRAM_APP_ID", ""),
         },
     )
     logger.info("Valid configuration found.")
